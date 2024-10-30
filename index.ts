94f28682f18de054848318ba588c4ce328fa642b
workingscorpion <workingscorpion@gmail.com>
2024-10-30 13:42:50 +0900

    feat: with options

--- index.ts
+++ index.ts
@@ -1,48 +1,7 @@
-import { generateFakeData, generateStub } from "./src/class-faker";
+import { generateStub } from "./src/class-faker";
 import { User } from "./src/user.class";
 
 // export * from "./src/class-faker";
 
-// const fieldNames: string[] = [];
-
-// function Field(target: any, propertyKey: string) {
-//   fieldNames.push(propertyKey);
-// }
-
-// Object.defineProperty(exports, "__esModule", { value: true });
-// console.log(
-//   "Object.getOwnPropertyNames(new User()) :>> ",
-//   Object.getOwnPropertyNames(new User())
-// );
-// const a = Object.getOwnPropertyNames(new User()).filter(
-//   (key) => typeof new User()[key] !== "function"
-// );
-// console.log("a :>> ", a);
-// // Object.getOwnPropertyNames(User.prototype).forEach((propertyKey) => {})
-
-// const result = generateStub<User>();
-
-// console.log("result :>> ", result);
-// console.log("fieldNames :>> ", fieldNames);
-
-// const fields = Reflect.getMetadata("fields", User.prototype);
-// console.log("Fields:", fields); // 필드 리스트 출력
-
-const fakeUser = generateFakeData(User);
+const fakeUser = generateStub(User);
 console.log("fakeUser :>> ", fakeUser);
-
-// generateStub(fields); // 스텁 생성
-
-// type Fields<T> = (keyof T)[];
-// console.log("typeof User :>> ", typeof User);
-// console.log('keyof User :>> ', keyof User);
-
-// 사용 예제
-// const userStub = generateStub(
-//   User
-//   //     {
-//   //   name: { skip: true }, // name은 생성을 건너뜀
-//   //   email: { type: "email" }, // email은 이메일 형태로 생성
-//   // }
-// );
-// console.log(userStub);
--- src/class-faker.ts
+++ src/class-faker.ts
@@ -1,7 +1,6 @@
-import { plainToInstance } from "class-transformer";
 import "reflect-metadata";
 import { faker } from "@faker-js/faker";
-import { getFieldMetadata } from "./faker-decorator";
+import { FieldMetadata, getFieldMetadata } from "./faker-decorator";
 
 // 필드 설정 타입 정의
 type FieldOptions<T> = {
@@ -10,109 +9,34 @@ type FieldOptions<T> = {
   value?: T; // specific value for this field
 };
 
-// 생성 함수
-export function generateStub<T>(): // entityClass: T
-// entityClass: new () => T
-// configs: Partial<Record<keyof T, FieldOptions<T[keyof T]>>> = {}
-T {
-  // console.log("entityClass :>> ", entityClass);
-  // console.log("typeof entityClass :>> ", typeof entityClass);
-  // const instance: T = {} as T;
-  // const instance = new entityClass();
-  // console.log("instance :>> ", instance);
-  // console.log("instance :>> ", instance);
-  // console.log("typeof instance :>> ", typeof instance);
-  // console.log(
-  //   "Object.getOwnPropertyNames(instance) :>> ",
-  //   Object.getOwnPropertyNames(instance)
-  // );
-  // console.log(
-  //   "Object.getOwnPropertyNames(entityClass) :>> ",
-  //   Object.getOwnPropertyNames(entityClass)
-  // );
-  // console.log("Object.keys(entityClass) :>> ", Object.keys(entityClass));
-  // console.log(
-  //   "Object.keys(new entityClass) :>> "
-  //   // Object.keys(new entityClass())
-  // );
-
-  // console.log("typeof entityClass :>> ", typeof entityClass);
-
-  // console.log("configs :>> ", configs);
-  // const instance = new entityClass();
-  // console.log("instance :>> ", instance);
-
-  // // 모든 메타데이터 키 가져오기
-  // // const propertyKeys = Object.getOwnPropertyNames(
-  // //   entityClass.prototype
-  // // ) as Array<keyof T>;
-  // // const propertyKeys = Object.getOwnPropertyNames(new entityClass()) as Array<
-  // //   keyof T
-  // // >;
-  // // // const metadataKeys = Reflect.getMetadataKeys(instance) as Array<keyof T>;
-  // const propertyKeys = Object.keys(instance) as Array<keyof T>;
-  // console.log("propertyKeys :>> ", propertyKeys);
-
-  // for (const propertyKey of propertyKeys) {
-  //   console.log("propertyKey :>> ", propertyKey);
-  //   const options = configs[propertyKey] || {}; // 필드별 옵션 가져오기
-  //   console.log("options :>> ", options);
-  //   const type = Reflect.getMetadata(
-  //     "design:type",
-  //     instance,
-  //     propertyKey as string
-  //   );
-
-  //   if (options.skip) {
-  //     // skip이 true면 생성하지 않음
-  //     continue;
-  //   }
-
-  //   console.log("options.value :>> ", options.value);
-  //   console.log("options.type :>> ", options.type);
-  //   console.log("propertyKey :>> ", propertyKey);
-  //   if (options.value !== undefined) {
-  //     // 수동 설정한 value가 있으면 우선 적용
-  //     instance[propertyKey] = options.value;
-  //   } else if (options.type === "email" && type === String) {
-  //     // 타입이 email로 지정된 경우 faker의 이메일 생성 함수 사용
-  //     instance[propertyKey] = faker.internet.email() as T[keyof T];
-  //   } else {
-  //     // type에 맞는 랜덤 데이터 생성 (기본 생성 로직)
-  //     if (type === String) {
-  //       instance[propertyKey] = faker.string.alpha() as T[keyof T];
-  //     } else if (type === Number) {
-  //       instance[propertyKey] = faker.number.int() as T[keyof T];
-  //     } else if (type === Boolean) {
-  //       instance[propertyKey] = faker.datatype.boolean() as T[keyof T];
-  //     }
-  //     // 필요시 추가 타입 핸들링
-  //   }
-  // }
-
-  return {} as T;
-  // return plainToInstance(entityClass, instance);
-}
-
-export function generateFakeData<T>(cls: new () => T): T {
+export function generateStub<T>(
+  cls: new () => T,
+  configs: Partial<Record<keyof T, FieldOptions<T[keyof T]>>> = {}
+): T {
   const instance = new cls();
   const fields = getFieldMetadata(instance);
 
-  console.log("fields :>> ", fields);
+  fields.forEach((field: FieldMetadata) => {
+    const { name, type } = field;
 
-  fields.forEach((field) => {
-    switch (field) {
-      case "name":
-        instance[field] = faker.string.alpha();
-        break;
-      case "email":
-        instance[field] = faker.internet.email();
-        break;
-      case "age":
-        instance[field] = faker.number.int({ min: 18, max: 99 });
-        break;
-      default:
-        instance[field] = null;
+    const options = configs[name] || {}; // 필드별 옵션 가져오기
+
+    if (options.value !== undefined) {
+      // 수동 설정한 value가 있으면 우선 적용
+      instance[name] = options.value;
+    } else if (options.type === "email" && type === String) {
+      // 타입이 email로 지정된 경우 faker의 이메일 생성 함수 사용
+      instance[name] = faker.internet.email() as T[keyof T];
+    } else {
+      // type에 맞는 랜덤 데이터 생성 (기본 생성 로직)
+      if (type === String) {
+        instance[name] = faker.string.alpha() as T[keyof T];
+      } else if (type === Number) {
+        instance[name] = faker.number.int() as T[keyof T];
+      } else if (type === Boolean) {
+        instance[name] = faker.datatype.boolean() as T[keyof T];
+      }
+      // 필요시 추가 타입 핸들링
     }
   });
 
